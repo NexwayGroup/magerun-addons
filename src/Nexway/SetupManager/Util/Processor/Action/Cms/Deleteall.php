21cf86ebe73eb02e8ecfeb30c36698f558e4b95f
Grzegorz Pawlik <grzegorzpawlik@gmail.com>
2015-09-01 09:32:17 +0200

    fixes after review

--- src/Nexway/SetupManager/Util/Processor/Action/Cms/Deleteall.php
+++ src/Nexway/SetupManager/Util/Processor/Action/Cms/Deleteall.php
@@ -8,8 +8,6 @@ use Nexway\SetupManager\Util\Processor\AbstractAction;
  *
  * @category     Nexway
  * @package      Nexway_SetupManager
- * @author       Mariusz Tasak <mtasak@nexway.com>
- * @copyright    Copyright (c) 2013-2014, Nexway
  */
 class Deleteall extends AbstractAction
 {
